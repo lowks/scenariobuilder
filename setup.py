2db0f969714a84e5f419a7c4c36cfe8c1faad626
Low Kian Seong <kianseong@gmail.com>
2014-11-23 18:36:11 +0800

    Update setup.py
    Adding a `description` string in `setup.py`

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='scenariobuilder',
     version='0.1.16',
+    description='Instantiates scenarios described by the scenario_node_terminus data model on an Openstack cloud',
     packages=['scenariobuilder'],
     license='Apache',
     url='https://github.com/CiscoSystems/scenariobuilder',
